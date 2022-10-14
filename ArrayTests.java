78b4c152b8e2cb671559869f366db35240aca0b6
Aure361 <114559967+Aure361@users.noreply.github.com>
2022-10-13 18:06:11 -0700

    update array

--- ArrayTests.java
+++ ArrayTests.java
@@ -30,7 +30,7 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
-  
+
   @Test
   public void manyItemsReverse() {
     int [] input1 = {1, 2, 3, 4, 5};
@@ -47,5 +47,12 @@ public class ArrayTests {
   public void oneZeroListReverse() {
     int [] input1 = {0};
     assertArrayEquals(new int[] {0}, ArrayExamples.reversed(input1));
+  }
+
+  //averageWithoutLowest tests
+  @Test
+  public void emptyList() {
+    double [] input1 = {};
+    assertEquals(0, ArrayExamples.averageWithoutLowest(input1), 0);
   }
 }
